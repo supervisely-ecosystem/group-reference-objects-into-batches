80b20aa5953b0af38e91d1287ac970ff915d79a6
max kolomeychenko <maxim.kolomeychenko@gmail.com>
2020-12-05 17:51:27 +0200

    grouping wip

--- src/main.py
+++ src/main.py
@@ -9,29 +9,39 @@ my_app = sly.AppService()
 TEAM_ID = int(os.environ['context.teamId'])
 WORKSPACE_ID = int(os.environ['context.workspaceId'])
 CATALOG_DF = None
+CATALOG_COLUMNS = []
+
 REFERENCE_PATHS = None
 REFERENCE_DATA = {}
+KEY_TAG_NAME = None
 
 
 @my_app.callback("preview_catalog")
 @sly.timeit
 def preview_catalog(api: sly.Api, task_id, context, state, app_logger):
-    global CATALOG_PATH, CATALOG_DF
+    global CATALOG_PATH, CATALOG_DF, CATALOG_COLUMNS
     catalog_path: str = state["catalogPath"]
     local_path = os.path.join(my_app.data_dir, catalog_path.lstrip("/"))
     try:
         api.file.download(TEAM_ID, catalog_path, local_path)
         catalog_df = pd.read_csv(local_path)
+        CATALOG_COLUMNS = [col for col in catalog_df.columns]
         catalog_df.insert(0, '#', list(range(len(catalog_df))))
         fields = [
             {"field": "data.catalog", "payload": json.loads(catalog_df.to_json(orient="split"))},
             {"field": "data.catalogError", "payload": ""},
+            {"field": "data.catalogColumns", "payload": CATALOG_COLUMNS},
+            {"field": "state.selectedColumn", "payload": CATALOG_COLUMNS[0]},
+            {"field": "state.groupingColumns", "payload": [False] * len(CATALOG_COLUMNS)},
         ]
         CATALOG_DF = catalog_df
     except Exception as e:
         fields = [
             {"field": "data.catalog", "payload": {"columns": [], "data": []}},
             {"field": "data.catalogError", "payload": repr(e)},
+            {"field": "data.catalogColumns", "payload": []},
+            {"field": "state.selectedColumn", "payload": ""},
+            {"field": "state.groupingColumns", "payload": []},
         ]
         CATALOG_DF = None
     api.app.set_fields(task_id, fields)
@@ -68,7 +78,10 @@ def preview_reference_files(api: sly.Api, task_id, context, state, app_logger):
 @my_app.callback("validate_reference_files")
 @sly.timeit
 def validate_reference_files(api: sly.Api, task_id, context, state, app_logger):
-    global REFERENCE_DATA
+    global REFERENCE_DATA, KEY_TAG_NAME
+    all_keys = set()
+    references_count = 0
+
     try:
         selected = state["referenceSelected"]
         for flag, remote_path in zip(selected, REFERENCE_PATHS):
@@ -76,13 +89,30 @@ def validate_reference_files(api: sly.Api, task_id, context, state, app_logger):
                 continue
             local_path = os.path.join(my_app.data_dir, remote_path.lstrip("/"))
             api.file.download(TEAM_ID, remote_path, local_path)
-            REFERENCE_DATA[remote_path] = sly.json.load_json_file(local_path)
+            cur_ref_data = sly.json.load_json_file(local_path)
+            if KEY_TAG_NAME is None:
+                KEY_TAG_NAME = cur_ref_data["key_tag_name"]
+            else:
+                if cur_ref_data["key_tag_name"] != KEY_TAG_NAME:
+                    raise ValueError("Key tag name {!r} in file {!r} differs from name in other files"
+                                     .format(cur_ref_data["key_tag_name"], remote_path))
+            REFERENCE_DATA[remote_path] = cur_ref_data
+            all_keys.update(cur_ref_data["all_keys"])
+            for k, v in cur_ref_data["references"].items():
+                references_count += len(v)
         fields = [
-            {"field": "data.referenceError", "payload": ""},
+            {"field": "data.referenceMessage", "payload": "Validation passed: {} keys has {} items in total"
+                                                          .format(len(all_keys), references_count)},
+            {"field": "data.messageColor", "payload": "green"},
+            {"field": "data.keyTagName", "payload": KEY_TAG_NAME},
+            {"field": "data.keysCount", "payload": len(all_keys)},
+            {"field": "data.referencesCount", "payload": references_count},
         ]
     except Exception as e:
         fields = [
-            {"field": "data.referenceError", "payload": repr(e)},
+            {"field": "data.referenceMessage", "payload": repr(e)},
+            {"field": "data.messageColor", "payload": "red"},
+            {"field": "data.keyTagName", "payload": ""},
         ]
         REFERENCE_DATA = {}
 
@@ -108,11 +138,17 @@ def main():
     data["catalog"] = {"columns": [], "data": []}
     data["catalogError"] = ""
     state["catalogPath"] = "/sample-data/products_01.csv"
+    data["catalogColumns"] = CATALOG_COLUMNS
+    state["selectedColumn"] = ""
+    state["groupingColumns"] = [False] * len(CATALOG_COLUMNS)
 
     state["referenceDir"] = "/reference_items/"
     data["referencePaths"] = []
-    data["referenceError"] = ""
+    data["referenceMessage"] = ""
+    data["messageColor"] = "red"
     state["referenceSelected"] = []
+    data["validationMsg"] = ""
+    data["keyTagName"] = ""
 
     state["perPage"] = 15,
     state["pageSizes"] = [10, 15, 30, 50, 100]
